a7a0a8d7dc9520b20208fa918111e115cc875eae
Takuma Ogawa <nyabo10_31@yahoo.co.jp>
2021-08-07 13:56:35 +0900

    UI部品の配置、コード紐付け

--- RegularExpressionsPrac/ViewController.swift
+++ RegularExpressionsPrac/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var targetStringTextField: UITextField!
+    @IBOutlet weak var regularExpressionsTextFiled: UITextField!
+    @IBOutlet weak var answerLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
+
+    @IBAction func TapCheckButton(_ sender: UIButton) {
+
+    }
 
 }
 
