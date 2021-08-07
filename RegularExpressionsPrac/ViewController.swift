302b0c89f29077644c727405d4dfe735856f31ab
Takuma Ogawa <nyabo10_31@yahoo.co.jp>
2021-08-07 14:52:14 +0900

    正規表現とテキストがいくつ合致しているかを確認できるようにした。

--- RegularExpressionsPrac/ViewController.swift
+++ RegularExpressionsPrac/ViewController.swift
@@ -7,6 +7,16 @@
 
 import UIKit
 
+extension String {
+    func regexMatch(pattern: String) -> Int {
+        guard let regex = try? NSRegularExpression(pattern: pattern, options: []) else {
+            return 0
+        }
+        let result = regex.matches(in: self, options: [], range: NSRange(0..<self.count))
+        return result.count
+    }
+}
+
 class ViewController: UIViewController {
 
     @IBOutlet weak var targetStringTextField: UITextField!
@@ -18,9 +28,15 @@ class ViewController: UIViewController {
         
     }
 
-
     @IBAction func TapCheckButton(_ sender: UIButton) {
-
+        let matchCounts = targetStringTextField.text!.regexMatch(pattern: regularExpressionsTextFiled.text!)
+        let answerLabelText: String
+        if matchCounts <= 0 {
+            answerLabelText = "対象なし"
+        } else {
+            answerLabelText = "\(matchCounts)つ当てはまる"
+        }
+        answerLabel.text = answerLabelText
     }
 
 }
